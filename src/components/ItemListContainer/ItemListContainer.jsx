1a56e12a67dde849d3e53562b32f83859f31d9ac
Sofia Ibarra <sofiaibarra415@gmail.com>
2024-07-22 20:16:08 -0300

    fix

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -4,7 +4,7 @@ import { getProducts, getProductsByCategory } from '../../data/asyncMock';
 import ItemList from '../ItemList/ItemList';
 import { useParams } from 'react-router-dom';
 import {ClipLoader} from 'react-spinners'
-import { Flex } from '@chakra-ui/react';
+import { Flex, Heading } from '@chakra-ui/react';
 import { db } from '../../config/firebase';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 
@@ -35,7 +35,7 @@ const ItemListContainer = ({ greeting }) => {
     }, [categoryId])
     return (
         <div className="item-list-container">
-            <h1 className='font'>{greeting}</h1>
+            <Heading color='#30546E' fontSize={25} m={5}>{greeting}</Heading>
             {
                 loading ?
                 <Flex justify={'center'} align={'center'} h={'50vh'}>
